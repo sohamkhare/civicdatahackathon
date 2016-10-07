db4621fb75ea3677da803a80c18f7f0f8da9d5c6
Soham Rajan Khare <srkhare@syr.edu>
2016-10-07 17:28:15 -0400

    Predictive modelling updated

--- predictiveModelling.R
+++ predictiveModelling.R
@@ -2,12 +2,20 @@
 install.packages("randomForest")
 library(randomForest)
 
-roadRatings2015 <- read.csv('//hd.ad.syr.edu/03/e1c9c9/Documents/Downloads/RoadRatings2015.csv')
-ratings.rf <- randomForest(overall ~ streetType + crack + patch + dateLastOverlay + flushOil + class + pavement ,data=roadRatings2015)
+roadRatings2015 <- read.csv('//hd.ad.syr.edu/03/e1c9c9/Documents/civicdatahackathon/roadRatingNew2015.csv')
+roadRatings2015 <- na.omit(roadRatings2015)
+
+ratings.rf <- randomForest(overall ~ streetType + crack + patch + dateLastOverlay + flushOilNum
+                           + classnum + pavement + NumofYrs ,data=roadRatings2015)
+
+ratings.rf <- randomForest(overall ~ streetType + crack + length + width + patch + dateLastOverlay + flushOilNum
+                           + classnum + pavement + NumofYrs ,data=roadRatings2015)
 
 str(roadRatings2015)
 (importance(ratings.rf))
 summary(ratings.rf)
-roadRatings2015 <- na.omit(roadRatings2015)
+
 summary(roadRatings2015)
 dim(roadRatings2015)
+
+p <- predict(rfm,test)
